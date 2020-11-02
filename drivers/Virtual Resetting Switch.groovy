29da2abd823eee6ad8a7921a0ce7a931e52476c1
waytotheweb <configserver@gmail.com>
2020-11-02 15:19:04 +0000

    Resetting Switch

---
+++ drivers/Virtual Resetting Switch.groovy
@@ -0,0 +1,32 @@
+/**
+ * Simple Virtual Resetting Switch Driver that works on
+ * Amazon Alexa to force on/off
+ *
+ *
+ * Version:  1.0
+ *
+ */
+metadata {
+    definition(name: "Virtual Resetting Switch", namespace: "waytotheweb", author: "Jonathan Michaelson") {
+        capability "Switch"
+
+	command "reset"
+    }
+}
+
+def on() {
+    log.info "on()"
+    sendEvent(name: "switch", value: "on")
+    reset()
+}
+
+def off() {
+    log.info "off()"
+    sendEvent(name: "switch", value: "off")
+    reset()
+}
+
+def reset() {
+    log.info "reset()"
+    sendEvent(name: "switch", value: "reset")
+}
