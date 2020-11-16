77e469219f664255bbfed811ad060542b6f815a6
waytotheweb <configserver@gmail.com>
2020-11-16 11:17:55 +0000

    Update XiaomiMijiaLightSensor.groovy

--- drivers/XiaomiMijiaLightSensor.groovy
+++ drivers/XiaomiMijiaLightSensor.groovy
@@ -1,5 +1,5 @@
 /**
- *  My Xiaomi Mijia Smart Light Sensor
+ *  Zigbee Xiaomi Mijia Smart Light Sensor
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
