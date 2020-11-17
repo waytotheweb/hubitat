543f4f80ea2c6a9f668fe037a77d7df779804744
waytotheweb <configserver@gmail.com>
2020-11-17 16:06:39 +0000

    Update XiaomiMijiaLightSensor.groovy

--- drivers/XiaomiMijiaLightSensor.groovy
+++ drivers/XiaomiMijiaLightSensor.groovy
@@ -1,21 +1,6 @@
 /**
  *  Xiaomi Mijia Smart Light Sensor
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- *	Current Contributer   : Jonathan Michaelson
- *	Original Contributors : John Constantelos (https://github.com/jsconstantelos/SmartThings/blob/master/devicetypes/jsconstantelos/my-xiaomi-mijia-smart-light-sensor.src/my-xiaomi-mijia-smart-light-sensor.groovy)
- *
- *  Updates:
- *  -------
- *  11-15-2020 : Initial commit, converted from ST and simplified
  */
 
 import hubitat.zigbee.zcl.DataType
@@ -30,7 +15,7 @@ metadata {
 
 		attribute "voltage", "number"
 
-		fingerprint profileId: "0104", inClusters: "0000,0400,0003,0001", outClusters: "0003", manufacturer: "LUMI", model: "lumi.sen_ill.mgl01", deviceJoinName: "Xiaomi Mijia Smart Home Light Sensor"
+		fingerprint profileId: "0104", inClusters: "0000,0001,0003,0400", outClusters: "0003", manufacturer: "LUMI", model: "lumi.sen_ill.mgl01", deviceJoinName: "Xiaomi Mijia Smart Home Light Sensor"
 
 	}
 	preferences {
